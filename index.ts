fb96e780eef53e3cf21a5ac06fdb5b549f7c04f2
youhei usami <usao9110@icloud.com>
2026-02-11 10:23:17 +0400

    2/11

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
-import * as http from "http";
+import express from "express";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { ContentType: "text/plain; charset=utf-8" });
-  res.end("Hello Node.js");
+const app = express();
+const PORT = 8890;
+
+app.get("/", (req, res) => {
+  res.send("Hello Web Server");
 });
 
-const PORT = 8890;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("サーバーが起動しました");
 });
+
+// import * as http from "http";
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { ContentType: "text/plain; charset=utf-8" });
+//   res.end("Hello Web server");
+// });
+
+// const PORT = 8890;
+// server.listen(PORT, () => {
+//   console.log("サーバーが起動しました");
+// });
